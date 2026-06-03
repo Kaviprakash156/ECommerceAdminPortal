5affef6165ad9c9f35b78df8f387852ae129042c
Kaviprakash <kaviprakash156@gmail.com>
2026-06-03 16:48:53 +0530

    Initial Commit

---
+++ ECommerceAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAdminPortal.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
